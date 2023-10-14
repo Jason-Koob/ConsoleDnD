215d3ef5bd1b0f4149085b2d3285cf8156957712
Jason Spears <64575009+Jason-Koob@users.noreply.github.com>
2023-10-14 03:32:02 -0400

    Player classes, rolls, stats and modifiers
    Added a name, description, hit die, primary ability stat, and save for each class. Changed how player rolls are created and used. Now each roll is individually added and when it is used, it is then removed from the list of available rolls to give to stats. Modifiers work the same way.

--- DnD.py
+++ DnD.py
@@ -1,53 +1,272 @@
 import random
+import os
+from art import *
+import time
+
 
 class playerInfo:
-        User = "Player_User"                     # The person playing the game
-        Name = "Player_name"                     # The name of the character
-        Class = "Player_class"                   # The class of the character
-        Level = 0                                # The level of the character
-        XP = 0                                   # The amount of experience a player has
-        AC = 0                                   # Character Armor Class
-        TotalHP = 8                              # Total Character Hitpoints
-        CurrentHP = 8                            # The current character Hitpoints
-        Speed = 0                                # Chacter Speed (ft)
-        CharacterAlignment = "Default_alignment" # Chacter social alignment used for choices
+    Name = "Player_name"                     # The name of the character
+    Class = "Player_class"                   # The class of the character
+    Level = 0                                # The level of the character
+    XP = 0                                   # The amount of experience a player has
+    AC = 0                                   # Character Armor Class
+    TotalHP = 0                              # Total Character Hitpoints
+    CurrentHP = 0                            # The current character Hitpoints
+    Speed = 0                                # Chacter Speed (ft)
+    # Chacter social alignment used for choices
+    CharacterAlignment = "Default_alignment"
+    Inventory = []
 
-class playerStats: # class for storing character stats
-        STR = random.randrange(3,18)
-        DEX = random.randrange(3,18)
-        CON = random.randrange(3,18)
-        INT = random.randrange(3,18)
-        WIS = random.randrange(3,18)
-        CHA = random.randrange(3,18)
 
-class playerModifier: # class for storing chacter modifers
-        STR = (playerStats.STR - 10)//2
-        DEX = (playerStats.DEX - 10)//2
-        CON = (playerStats.CON - 10)//2
-        INT = (playerStats.INT - 10)//2
-        WIS = (playerStats.WIS - 10)//2
-        CHA = (playerStats.CHA - 10)//2
+playerRolls = []
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+
+playerRollsModifer = []
+
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
+
+class playerStats:  # class for storing character stats
+    STR = 0
+    DEX = 0
+    CON = 0
+    INT = 0
+    WIS = 0
+    CHA = 0
+
+
+class playerModifier:  # class for storing chacter modifers
+    STR = (playerStats.STR - 10)//2
+    DEX = (playerStats.DEX - 10)//2
+    CON = (playerStats.CON - 10)//2
+    INT = (playerStats.INT - 10)//2
+    WIS = (playerStats.WIS - 10)//2
+    CHA = (playerStats.CHA - 10)//2
+
 
 class world:
-        regionChoice = ["Argonia", "Scandanavia", "The Forgotten Realms", "Brazil", "Estonia", "Stronghelm"]  # List of possible regions for the player to play in
-        locChoice = ["tavern", "library", "blacksmith", "apothicary", "townhall", "sewer system"]             # List of possible buildings within a town to play in
-        NPC_names = ["Garzog", "Skeeb", "Angus", "Yonna", "John", "Pengu"]
-        region = random.choice(regionChoice)
-        location1 = random.choice(locChoice)
-        shopkeeper1 = random.choice(NPC_names)
-        location2 = random.choice(locChoice)
-        shopkeeper2 = random.choice(NPC_names)
+    regionChoice = ["Argonia", "Scandanavia", "The Forgotten Realms", "Brazil",
+                    "Estonia", "Stronghelm"]  # List of possible regions for the player to play in
+    # List of possible buildings within a town to play in
+    locChoice = ["tavern", "library", "blacksmith",
+                 "apothicary", "townhall", "sewer system"]
+    # List of possible names for NPCs around town
+    NPC_names = ["Garzog", "Lydia", "Angus", "Yonna", "John", "Pengu", "Skeeb"]
+    region = random.choice(regionChoice)
+    location1 = random.choice(locChoice)
+    shopkeeper1 = random.choice(NPC_names)
+    location2 = random.choice(locChoice)
+    shopkeeper2 = random.choice(NPC_names)
+
 
 class story:
-        mainChoice = ["recover stolen goods", "kill some mischevious goblins", "recover a relgious artifact"] # List of possible main quests
-        mainquest = random.choice(mainChoice)                                                                 # Determines main quest of the story
+    mainChoice = ["recover stolen goods", "kill some mischevious goblins",
+                  "recover a relgious artifact"]  # List of possible main quests
+    # Determines main quest of the story
+    mainquest = random.choice(mainChoice)
 
-# Testing
-print(playerStats.STR,    playerStats.DEX,    playerStats.CON,    playerStats.INT,    playerStats.WIS,    playerStats.CHA)
-print(playerModifier.STR, playerModifier.DEX, playerModifier.CON, playerModifier.INT, playerModifier.WIS, playerModifier.CHA)
-print("Main Quest:       " +story.mainquest)
-print("World:            " + world.region)
-print("shopkeep1:        " + world.shopkeeper1)
-print("Location1:        " + world.location1)
-print("shopkeep2:        " + world.shopkeeper2)
-print("Location2:        " + world.location2)
+
+# ---------------------- START ---------------------- #
+os.system('cls')
+tprint("Command Line\nDungeons and Dragons", font="Cybermedium")
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress Enter to continue.")
+option = input()
+if option == "":
+    os.system('cls')
+
+# Naming the character
+tprint("Naming your\ncharacter", font="Cybermedium")
+print("What would you like your character to be called?\n")
+
+playerInfo.Name = str(input())
+os.system('cls')
+
+# Describing each character class
+tprint("Choosing your\ncharacter class", font="Cybermedium")
+print("Now it's time to choose a character class. Here are all the classes to choose from in DnD.\nEach class has a description of what they do and how they do it.\n\n The hit die is used to get your initial hit points and is used to determine how much health is healed/gained.\n The primary ability is the ability or stat that is used to most in that class.\n The saves stat is used to determine if you character lives or dies when fatally wounded.")
+time.sleep(15)
+print("\n\n--------- Wizard ----------")
+print("A scholarly magic-user capable of manipulating the structure of reality.\nHit Die: d6\nPrimary Ability: Intelligence\nSaves: Intelligence & Wisdom")
+time.sleep(5)
+print("\n\n--------- Warlock ---------")
+print("A wielder of magic that is derived from a bargin with an extraplanar entity.\nHit Die: d8\nPrimary Ability: Charisma\nSaves: Wisdom & Charisma")
+time.sleep(5)
+print("\n\n--------- Sorcerer --------")
+print("A spellcaster who draws on inherent magic from a gift or bloodline.\nHit Die: d6\nPrimary Ability: Charisma\nSaves: Constitution & Charisma")
+time.sleep(5)
+print("\n\n--------- Rogue -----------")
+print("A scondrel who uses stealth and trickery to overcome obstacles and enemies.\nHit Die: d8\nPrimary Ability: Dexterity\nSaves: Dexterity & Intelligence")
+time.sleep(5)
+print("\n\n--------- Ranger ----------")
+print("A warrior who combats threats on the edges of civilization.\nHit Die: d10\nPrimary Ability: Dexterity & Wisdom\nSaves: Strength & Dexterity")
+time.sleep(5)
+print("\n\n--------- Paladin ---------")
+print("A holy warrior bound to a sacred oath.\nHit Die: d10\nPrimary Ability: Strength & Charisma\nSaves: Wisdom & Charisma")
+time.sleep(5)
+print("\n\n--------- Monk ------------")
+print("A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection.\nHit Die: d8\nPrimary Abiliy: Dexterity & Wisdom\nSaves: Strength and Dexterity")
+time.sleep(5)
+print("\n\n--------- Fighter ---------")
+print("A master of martial combat, skilled with a variety of weapons and armor.\nHit Die: d10\nPrimary Ability: Strength or Dexterity\nSaves: Strength & Constitution")
+time.sleep(5)
+print("\n\n--------- Druid -----------")
+print("A priest of the Old Faith, wielding the powers of nature and adopting animal forms.\nHit Die: d8\nPrimary Ability: Wisdom\nSaves: Intelligence & Wisdom")
+time.sleep(5)
+print("\n\n--------- Cleric ----------")
+print("A priestly champion who wields divine magic in service of a higher power.\nHit Die: d8\nPrimary Ability: Wisdom\nSaves: Wisdom & Charisma")
+time.sleep(5)
+print("\n\n--------- Bard ------------")
+print("An inspiring magician whose power echoes the music of creation.\nHit Die: d8\nPrimary Ability: Charisma\nSaves: Dexterity & Charisma")
+time.sleep(5)
+print("\n\n--------- Barbarian -------")
+print("A fierce warrior who can enter a battle rage.\nHit Die: d12\nPrimary Ability: Strength\nSaves: Strength & Constitution")
+time.sleep(5)
+print("\n\n--------- Artificer -------")
+print("Masters of invention, artificers use ingenuity and magic to unlokc extraordinary capabilities in objects.\nHit Die: d8\nPrimary Ability: Intelligence\nSaves: Constitution & Intelligence")
+
+# Choosing a character class
+time.sleep(2)
+print("\n\nOut of those, which would you like to choose?\n")
+option = input()
+
+if option == "Wizard" or option == "wizard":
+    playerInfo.Class = "Wizard"
+elif option == "Warlock" or option == "warlock":
+    playerInfo.Class = "Warlock"
+elif option == "Sorcerer" or option == "sorcerer":
+    playerInfo.Class = "Sorcerer"
+elif option == "Rogue" or option == "rogue":
+    playerInfo.Class = "Rogue"
+elif option == "Ranger" or option == "ranger":
+    playerInfo.Class = "Ranger"
+elif option == "Paladin" or option == "paladin":
+    playerInfo.Class = "Paladin"
+elif option == "Monk" or option == "monk":
+    playerInfo.Class = "Monk"
+elif option == "Fighter" or option == "fighter":
+    playerInfo.Class = "Fighter"
+elif option == "Druid" or option == "druid":
+    playerInfo.Class = "Druid"
+elif option == "Cleric" or option == "cleric":
+    playerInfo.Class = "Cleric"
+elif option == "Bard" or option == "bard":
+    playerInfo.Class = "Bard"
+elif option == "Barbarian" or option == "barb":
+    playerInfo.Class = "Barbarian"
+elif option == "Artificer" or option == "artificer":
+    playerInfo.Class = "Artificer"
+else:
+    print("Please choose a class.")
+
+# Stat distribution
+tprint("Character Stats\nand modifers", font="Cybermedium")
+print("Normally you would roll a 6-sided die (d6) 4 times and remove the lowest value and then choose which stat get which number.")
+print("The number called the modifer is applied to your dice rolls and changes your rolls for better or worse.\n\n")
+time.sleep(10)
+print("Strength (STR) - Being able to crush a tomato\n")
+time.sleep(2)
+print("Dexterity (DEX) - Being able to dodge a tomato\n")
+time.sleep(2)
+print("Constitution (CON) - Being able to eat a bad tomato\n")
+time.sleep(2)
+print("Intelligence (INT) - Knowing the signs of a bad tomato\n")
+time.sleep(2)
+print("Wisdom (WIS) - The ability to notice the signs of a bad tomato\n")
+time.sleep(2)
+print("Charisma (CHA) - Being able to sell a bad tomato\n")
+time.sleep(2)
+
+print("\nYour rolls to distribute are:\n" + str(playerRolls) +
+      "\nThe modifers going along with these rolls are: \n" + str(playerRollsModifer))
+
+print("\n\nWhich roll will go to Strength?\n")
+option = input()
+RollLocation = playerRolls.index(int(option))
+playerRolls.pop(RollLocation)
+playerStats.STR = option
+playerModifier.STR = (int(playerStats.STR) - 10)//2
+
+playerRollsModifer.clear()
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
+print("\nROLLS:\n" + str(playerRolls) +
+      "\nMODS: \n" + str(playerRollsModifer))
+
+print("\n\nWhich roll will go to Dexterity?\n")
+option = input()
+RollLocation = playerRolls.index(int(option))
+playerRolls.pop(RollLocation)
+playerStats.DEX = option
+playerModifier.DEX = (int(playerStats.DEX) - 10)//2
+
+playerRollsModifer.clear()
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
+print("\nROLLS:\n" + str(playerRolls) +
+      "\nMODS: \n" + str(playerRollsModifer))
+
+print("\n\nWhich roll will go to Constitution?\n")
+option = input()
+RollLocation = playerRolls.index(int(option))
+playerRolls.pop(RollLocation)
+playerStats.CON = option
+playerModifier.CON = (int(playerStats.CON) - 10)//2
+
+playerRollsModifer.clear()
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
+print("\nROLLS:\n" + str(playerRolls) +
+      "\nMODS: \n" + str(playerRollsModifer))
+
+print("\n\nWhich roll will go to Intelligence?\n")
+option = input()
+RollLocation = playerRolls.index(int(option))
+playerRolls.pop(RollLocation)
+playerStats.INT = option
+playerModifier.INT = (int(playerStats.INT) - 10)//2
+
+playerRollsModifer.clear()
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
+print("\nROLLS:\n" + str(playerRolls) +
+      "\nMODS: \n" + str(playerRollsModifer))
+
+print("\n\nWhich roll will go to Wisdom?\n")
+option = input()
+RollLocation = playerRolls.index(int(option))
+playerRolls.pop(RollLocation)
+playerStats.WIS = option
+playerModifier.WIS = (int(playerStats.WIS) - 10)//2
+
+playerRollsModifer.clear()
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
+print("\nROLLS:\n" + str(playerRolls) +
+      "\nMODS: \n" + str(playerRollsModifer))
+
+print("\n\nWhich roll will go to Charisma?\n")
+option = input()
+RollLocation = playerRolls.index(int(option))
+playerRolls.pop(RollLocation)
+playerStats.CHA = option
+playerModifier.CHA = (int(playerStats.CHA) - 10)//2
+
+# Print Stats
+print("Your stats are: \n"
+      + "STR: " + str(playerStats.STR) + " (" + str(playerModifier.STR) + ")\n"
+      + "DEX: " + str(playerStats.DEX) + " (" + str(playerModifier.DEX) + ")\n"
+      + "CON: " + str(playerStats.CON) + " (" + str(playerModifier.CON) + ")\n"
+      + "INT: " + str(playerStats.INT) + " (" + str(playerModifier.INT) + ")\n"
+      + "WIS: " + str(playerStats.WIS) + " (" + str(playerModifier.WIS) + ")\n"
+      + "CHA: " + str(playerStats.CHA) + " (" + str(playerModifier.CHA) + ")\n")
