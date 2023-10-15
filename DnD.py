eed69bd67879b014096a430c9b9cd9fcb1e290fe
Jason Spears <64575009+Jason-Koob@users.noreply.github.com>
2023-10-14 22:40:31 -0400

    Made some class info
    Made class info for each class, fixed

--- DnD.py
+++ DnD.py
@@ -19,20 +19,6 @@ class playerInfo:
     Inventory = []
 
 
-playerRolls = []
-playerRolls.append(random.randrange(3, 18))
-playerRolls.append(random.randrange(3, 18))
-playerRolls.append(random.randrange(3, 18))
-playerRolls.append(random.randrange(3, 18))
-playerRolls.append(random.randrange(3, 18))
-playerRolls.append(random.randrange(3, 18))
-
-playerRollsModifer = []
-
-for rolls in playerRolls:
-    playerRollsModifer.append((int(rolls) - 10)//2)
-
-
 class playerStats:  # class for storing character stats
     STR = 0
     DEX = 0
@@ -73,13 +59,24 @@ class story:
     mainquest = random.choice(mainChoice)
 
 
+playerRolls = []
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+playerRolls.append(random.randrange(3, 18))
+
+playerRollsModifer = []
+
+for rolls in playerRolls:
+    playerRollsModifer.append((int(rolls) - 10)//2)
+
 # ---------------------- START ---------------------- #
-os.system('cls')
 tprint("Command Line\nDungeons and Dragons", font="Cybermedium")
 print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nPress Enter to continue.")
 option = input()
-if option == "":
-    os.system('cls')
+os.system('cls')
 
 # Naming the character
 tprint("Naming your\ncharacter", font="Cybermedium")
@@ -89,100 +86,130 @@ playerInfo.Name = str(input())
 os.system('cls')
 
 # Describing each character class
-tprint("Choosing your\ncharacter class", font="Cybermedium")
-print("Now it's time to choose a character class. Here are all the classes to choose from in DnD.\nEach class has a description of what they do and how they do it.\n\n The hit die is used to get your initial hit points and is used to determine how much health is healed/gained.\n The primary ability is the ability or stat that is used to most in that class.\n The saves stat is used to determine if you character lives or dies when fatally wounded.")
-time.sleep(15)
+tprint("Choosing your\ncharacter's class", font="Cybermedium")
+print("Now it's time to choose a character class. Here are all the classes to choose from in DnD.\nEach class has a description of what they do and how they do it.\n\n The hit die is used to get your initial hit points and is used to determine how much health is healed/gained.\n The primary ability is the ability or stat that is used to most in that class.\n The saves stat is used to determine if you character lives or dies when fatally wounded.\n")
+time.sleep(1)
 print("\n\n--------- Wizard ----------")
 print("A scholarly magic-user capable of manipulating the structure of reality.\nHit Die: d6\nPrimary Ability: Intelligence\nSaves: Intelligence & Wisdom")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Warlock ---------")
 print("A wielder of magic that is derived from a bargin with an extraplanar entity.\nHit Die: d8\nPrimary Ability: Charisma\nSaves: Wisdom & Charisma")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Sorcerer --------")
 print("A spellcaster who draws on inherent magic from a gift or bloodline.\nHit Die: d6\nPrimary Ability: Charisma\nSaves: Constitution & Charisma")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Rogue -----------")
 print("A scondrel who uses stealth and trickery to overcome obstacles and enemies.\nHit Die: d8\nPrimary Ability: Dexterity\nSaves: Dexterity & Intelligence")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Ranger ----------")
 print("A warrior who combats threats on the edges of civilization.\nHit Die: d10\nPrimary Ability: Dexterity & Wisdom\nSaves: Strength & Dexterity")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Paladin ---------")
 print("A holy warrior bound to a sacred oath.\nHit Die: d10\nPrimary Ability: Strength & Charisma\nSaves: Wisdom & Charisma")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Monk ------------")
 print("A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection.\nHit Die: d8\nPrimary Abiliy: Dexterity & Wisdom\nSaves: Strength and Dexterity")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Fighter ---------")
 print("A master of martial combat, skilled with a variety of weapons and armor.\nHit Die: d10\nPrimary Ability: Strength or Dexterity\nSaves: Strength & Constitution")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Druid -----------")
 print("A priest of the Old Faith, wielding the powers of nature and adopting animal forms.\nHit Die: d8\nPrimary Ability: Wisdom\nSaves: Intelligence & Wisdom")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Cleric ----------")
 print("A priestly champion who wields divine magic in service of a higher power.\nHit Die: d8\nPrimary Ability: Wisdom\nSaves: Wisdom & Charisma")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Bard ------------")
 print("An inspiring magician whose power echoes the music of creation.\nHit Die: d8\nPrimary Ability: Charisma\nSaves: Dexterity & Charisma")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Barbarian -------")
 print("A fierce warrior who can enter a battle rage.\nHit Die: d12\nPrimary Ability: Strength\nSaves: Strength & Constitution")
-time.sleep(5)
+time.sleep(3)
 print("\n\n--------- Artificer -------")
-print("Masters of invention, artificers use ingenuity and magic to unlokc extraordinary capabilities in objects.\nHit Die: d8\nPrimary Ability: Intelligence\nSaves: Constitution & Intelligence")
+print("Masters of invention, artificers use ingenuity and magic to unlock extraordinary capabilities in objects.\nHit Die: d8\nPrimary Ability: Intelligence\nSaves: Constitution & Intelligence")
 
 # Choosing a character class
-time.sleep(2)
+time.sleep(1)
 print("\n\nOut of those, which would you like to choose?\n")
-option = input()
 
-if option == "Wizard" or option == "wizard":
-    playerInfo.Class = "Wizard"
-elif option == "Warlock" or option == "warlock":
-    playerInfo.Class = "Warlock"
-elif option == "Sorcerer" or option == "sorcerer":
-    playerInfo.Class = "Sorcerer"
-elif option == "Rogue" or option == "rogue":
-    playerInfo.Class = "Rogue"
-elif option == "Ranger" or option == "ranger":
-    playerInfo.Class = "Ranger"
-elif option == "Paladin" or option == "paladin":
-    playerInfo.Class = "Paladin"
-elif option == "Monk" or option == "monk":
-    playerInfo.Class = "Monk"
-elif option == "Fighter" or option == "fighter":
-    playerInfo.Class = "Fighter"
-elif option == "Druid" or option == "druid":
-    playerInfo.Class = "Druid"
-elif option == "Cleric" or option == "cleric":
-    playerInfo.Class = "Cleric"
-elif option == "Bard" or option == "bard":
-    playerInfo.Class = "Bard"
-elif option == "Barbarian" or option == "barb":
-    playerInfo.Class = "Barbarian"
-elif option == "Artificer" or option == "artificer":
-    playerInfo.Class = "Artificer"
-else:
-    print("Please choose a class.")
+classChoosing = True
+
+while classChoosing == True:
+    option = input()
+    if option == "Wizard" or option == "wizard":
+        playerInfo.Class = "Wizard"
+        classChoosing = False
+
+    if option == "Warlock" or option == "warlock":
+        playerInfo.Class = "Warlock"
+        classChoosing = False
+
+    if option == "Sorcerer" or option == "sorcerer":
+        playerInfo.Class = "Sorcerer"
+        classChoosing = False
+
+    if option == "Rogue" or option == "rogue":
+        playerInfo.Class = "Rogue"
+        classChoosing = False
+
+    if option == "Ranger" or option == "ranger":
+        playerInfo.Class = "Ranger"
+        classChoosing = False
+
+    if option == "Paladin" or option == "paladin":
+        playerInfo.Class = "Paladin"
+        classChoosing = False
+
+    if option == "Monk" or option == "monk":
+        playerInfo.Class = "Monk"
+        classChoosing = False
+
+    if option == "Fighter" or option == "fighter":
+        playerInfo.Class = "Fighter"
+        classChoosing = False
+
+    if option == "Druid" or option == "druid":
+        playerInfo.Class = "Druid"
+        classChoosing = False
+
+    if option == "Cleric" or option == "cleric":
+        playerInfo.Class = "Cleric"
+        classChoosing = False
+
+    if option == "Bard" or option == "bard":
+        playerInfo.Class = "Bard"
+        classChoosing = False
+
+    if option == "Barbarian" or option == "barb":
+        playerInfo.Class = "Barbarian"
+        classChoosing = False
+
+    if option == "Artificer" or option == "artificer":
+        playerInfo.Class = "Artificer"
+        classChoosing = False
+
+    else:
+        print()
 
 # Stat distribution
 tprint("Character Stats\nand modifers", font="Cybermedium")
-print("Normally you would roll a 6-sided die (d6) 4 times and remove the lowest value and then choose which stat get which number.")
+print("Selected class: " + playerInfo.Class + "\n")
+print("Normally you would roll a 6-sided die (d6) 4 times and remove the lowest value.\nThese numbers are then distributed to different stats choosen by the player(you).")
 print("The number called the modifer is applied to your dice rolls and changes your rolls for better or worse.\n\n")
 # Describing what a stat is used for
 time.sleep(10)
 print("Strength (STR) - Being able to crush a tomato\n")
-time.sleep(2)
+time.sleep(1)
 print("Dexterity (DEX) - Being able to dodge a tomato\n")
-time.sleep(2)
+time.sleep(1)
 print("Constitution (CON) - Being able to eat a bad tomato\n")
-time.sleep(2)
+time.sleep(1)
 print("Intelligence (INT) - Knowing the signs of a bad tomato\n")
-time.sleep(2)
+time.sleep(1)
 print("Wisdom (WIS) - The ability to notice the signs of a bad tomato\n")
-time.sleep(2)
+time.sleep(1)
 print("Charisma (CHA) - Being able to sell a bad tomato\n")
-time.sleep(2)
+time.sleep(1)
 
 print("\nYour rolls to distribute are:\n" + str(playerRolls) +  # prints how many rolls are left
       "\nThe modifers going along with these rolls are: \n" + str(playerRollsModifer))  # Prints each modifer for each roll left
@@ -276,3 +303,4 @@ print("Your stats are: \n"
       + "INT: " + str(playerStats.INT) + " (" + str(playerModifier.INT) + ")\n"
       + "WIS: " + str(playerStats.WIS) + " (" + str(playerModifier.WIS) + ")\n"
       + "CHA: " + str(playerStats.CHA) + " (" + str(playerModifier.CHA) + ")\n")
+time.sleep(5)
--- dndClasses.py
+++ dndClasses.py
@@ -1,4 +1,5 @@
 import random
+import DnD
 
 
 class stat2skill:
@@ -23,56 +24,116 @@ class stat2skill:
 
 class Wizard:
     HitDie = random.randrange(1, 6)
-    Armor = 0
-    SimpleWeapons = ["Dagger", "Darts", "Slings",
-                     "Quarterstaff", 'Light Crossbow']
-    SimpleWeaponDamage = [random.randrange(1, 4), random.randrange(
-        1, 4), random.randrange(1, 4), random.randrange(1, 6), random.randrange(1, 8)]
-    SimpleWeaponAction = ["throw", "shoot", "sling", "hit", "shoot"]
-    Tools = 0
-    Skills = ["Arcana", "History", "Insight",
-              "Investigation", "Medicine", "Religion"]
-    StartingInventory = [SimpleWeapons[4], "Component Pouch", "Backpack", "Bedroll", "mess kit",
-                         "tinderbox", "10 torches", "10 days of rations", "a waterskin", "50 ft of hempen rope"]
-    CantripLimit = 3
-    TotalCantrips = ["Acid Splash", "Blade Ward", "Booming Blade", "Chill touch", "Control Flames", "Create Bonfire", "Dancing Flames", "Fire Bolt", "Friends", "Frostbite", "Green-Flame Blade", "Gust", "Infestastion", "Light", "Lightning Lure",
-                     "Mage Hand", "Mending", "Message", "Mind Silver", "Minor Illusion", "Mold Earth", "Poison Spray", "Prestidigitation", "Ray of Frost", "Shape Water", "Shocking Grasp", "Sword Burst", "Thunderclap", "Toll the Dead", "True Strike"]
-    TotalCantripDescriptions = [""]
-    TotalCantripDamage = []
-    TotalCantripRange = [60, 0, 5, 120, 60, 60, 120, 120, 0, 60, 5, 30,
-                         30, 5, 15, 30, 5, 120, 60, 30, 30, 10, 10, 60, 30, 5, 5, 5, 60, 30]
-    SpellSlots = 2
-    SpellsKnown = 6
-    TotalSpells = []
-    TotalSpellsDescriptions = []
-    TotalSpellsDamage = []
-    TotalSpellsRange = []
+    PrimaryStat = DnD.playerStats.INT
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.WIS, DnD.playerStats.CHA]
+    Skills = stat2skill
 
-    print(len(TotalCantrips))
-    print(len(TotalCantripRange))
 
-    class Warlock:
-    
-    class Sorcerer:
-    
-    class Rogue:
+class Warlock:
+    HitDie = random.randrange(1, 6)
+    PrimaryStat = DnD.playerStats.INT
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.INT, DnD.playerStats.WIS]
+    Skills = stat2skill
 
-    class Ranger:
 
-    class Paladin:
+class Sorcerer:
+    HitDie = random.randrange(1, 6)
+    PrimaryStat = DnD.playerStats.CHA
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.CON, DnD.playerStats.CHA]
+    Skills = stat2skill
 
-    class Monk:
 
-    class Fighter:
+class Rogue:
+    HitDie = random.randrange(1, 8)
+    PrimaryStat = DnD.playerStats.DEX
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.DEX, DnD.playerStats.INT]
+    Skills = stat2skill
 
-    class Druid:
 
-    class Cleric:
+class Ranger:
+    HitDie = random.randrange(1, 10)
+    PrimaryStat = DnD.playerStats.DEX, DnD.playerStats.WIS
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.STR, DnD.playerStats.DEX]
+    Skills = stat2skill
 
-    class Bard:
 
-    class Barbarian:
+class Paladin:
+    HitDie = random.randrange(1, 10)
+    PrimaryStat = DnD.playerStats.STR, DnD.playerStats.CHA
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.WIS, DnD.playerStats.CHA]
+    Skills = stat2skill
 
-    class Artificer:
 
-      
+class Monk:
+    HitDie = random.randrange(1, 8)
+    PrimaryStat = DnD.playerStats.DEX, DnD.playerStats.WIS
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.STR, DnD.playerStats.DEX]
+    Skills = stat2skill
+
+
+class Fighter:
+    HitDie = random.randrange(1, 10)
+    PrimaryStat = DnD.playerStats.STR, DnD.playerStats.DEX
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.STR, DnD.playerStats.CON]
+    Skills = stat2skill
+
+
+class Druid:
+    HitDie = random.randrange(1, 8)
+    PrimaryStat = DnD.playerStats.WIS
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.INT, DnD.playerStats.WIS]
+    Skills = stat2skill
+
+
+class Cleric:
+    HitDie = random.randrange(1, 8)
+    PrimaryStat = DnD.playerStats.WIS
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.WIS, DnD.playerStats.CHA]
+    Skills = stat2skill
+
+
+class Bard:
+    HitDie = random.randrange(1, 8)
+    PrimaryStat = DnD.playerStats.CHA
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.DEX, DnD.playerStats.CHA]
+    Skills = stat2skill
+
+
+class Barbarian:
+    HitDie = random.randrange(1, 12)
+    PrimaryStat = DnD.playerStats.STR
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.STR, DnD.playerStats.CON]
+    Skills = stat2skill
+
+
+class Artificer:
+    HitDie = random.randrange(1, 8)
+    PrimaryStat = DnD.playerStats.INT
+    SpellSlots = 0
+    TotalCantrips = 0
+    Saves = [DnD.playerStats.CON, DnD.playerStats.INT]
+    Skills = stat2skill
