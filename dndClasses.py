e31e67e13bf983d2a15822ebe294bc42462e7bb9
Jason Spears <64575009+Jason-Koob@users.noreply.github.com>
2023-10-15 14:23:13 -0400

    Contained classes within charClass
    Easier to import into DnD.py file

--- dndClasses.py
+++ dndClasses.py
@@ -88,118 +88,108 @@ class Race:
 # print(Race.Tiefling.Stats)
 
 # Classes for each character class
-class Wizard:
-    HitDie = random.randrange(1, 6)
-    PrimaryStat = playerStats.INT
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.WIS, playerStats.CHA]
-    Skills = stat2skill
+class charClass:
 
+    class Wizard:
+        HitDie = random.randrange(1, 6)
+        PrimaryStat = playerStats.INT
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.WIS, playerStats.CHA]
+        Skills = stat2skill
 
-class Warlock:
-    HitDie = random.randrange(1, 6)
-    PrimaryStat = playerStats.INT
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.INT, playerStats.WIS]
-    Skills = stat2skill
+    class Warlock:
+        HitDie = random.randrange(1, 6)
+        PrimaryStat = playerStats.INT
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.INT, playerStats.WIS]
+        Skills = stat2skill
 
+    class Sorcerer:
+        HitDie = random.randrange(1, 6)
+        PrimaryStat = playerStats.CHA
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.CON, playerStats.CHA]
+        Skills = stat2skill
 
-class Sorcerer:
-    HitDie = random.randrange(1, 6)
-    PrimaryStat = playerStats.CHA
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.CON, playerStats.CHA]
-    Skills = stat2skill
+    class Rogue:
+        HitDie = random.randrange(1, 8)
+        PrimaryStat = playerStats.DEX
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.DEX, playerStats.INT]
+        Skills = stat2skill
 
+    class Ranger:
+        HitDie = random.randrange(1, 10)
+        PrimaryStat = playerStats.DEX, playerStats.WIS
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.STR, playerStats.DEX]
+        Skills = stat2skill
 
-class Rogue:
-    HitDie = random.randrange(1, 8)
-    PrimaryStat = playerStats.DEX
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.DEX, playerStats.INT]
-    Skills = stat2skill
+    class Paladin:
+        HitDie = random.randrange(1, 10)
+        PrimaryStat = playerStats.STR, playerStats.CHA
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.WIS, playerStats.CHA]
+        Skills = stat2skill
 
+    class Monk:
+        HitDie = random.randrange(1, 8)
+        PrimaryStat = playerStats.DEX, playerStats.WIS
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.STR, playerStats.DEX]
+        Skills = stat2skill
 
-class Ranger:
-    HitDie = random.randrange(1, 10)
-    PrimaryStat = playerStats.DEX, playerStats.WIS
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.STR, playerStats.DEX]
-    Skills = stat2skill
+    class Fighter:
+        HitDie = random.randrange(1, 10)
+        PrimaryStat = playerStats.STR, playerStats.DEX
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.STR, playerStats.CON]
+        Skills = stat2skill
 
+    class Druid:
+        HitDie = random.randrange(1, 8)
+        PrimaryStat = playerStats.WIS
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.INT, playerStats.WIS]
+        Skills = stat2skill
 
-class Paladin:
-    HitDie = random.randrange(1, 10)
-    PrimaryStat = playerStats.STR, playerStats.CHA
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.WIS, playerStats.CHA]
-    Skills = stat2skill
+    class Cleric:
+        HitDie = random.randrange(1, 8)
+        PrimaryStat = playerStats.WIS
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.WIS, playerStats.CHA]
+        Skills = stat2skill
 
+    class Bard:
+        HitDie = random.randrange(1, 8)
+        PrimaryStat = playerStats.CHA
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.DEX, playerStats.CHA]
+        Skills = stat2skill
 
-class Monk:
-    HitDie = random.randrange(1, 8)
-    PrimaryStat = playerStats.DEX, playerStats.WIS
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.STR, playerStats.DEX]
-    Skills = stat2skill
+    class Barbarian:
+        HitDie = random.randrange(1, 12)
+        PrimaryStat = playerStats.STR
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.STR, playerStats.CON]
+        Skills = stat2skill
 
-
-class Fighter:
-    HitDie = random.randrange(1, 10)
-    PrimaryStat = playerStats.STR, playerStats.DEX
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.STR, playerStats.CON]
-    Skills = stat2skill
-
-
-class Druid:
-    HitDie = random.randrange(1, 8)
-    PrimaryStat = playerStats.WIS
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.INT, playerStats.WIS]
-    Skills = stat2skill
-
-
-class Cleric:
-    HitDie = random.randrange(1, 8)
-    PrimaryStat = playerStats.WIS
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.WIS, playerStats.CHA]
-    Skills = stat2skill
-
-
-class Bard:
-    HitDie = random.randrange(1, 8)
-    PrimaryStat = playerStats.CHA
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.DEX, playerStats.CHA]
-    Skills = stat2skill
-
-
-class Barbarian:
-    HitDie = random.randrange(1, 12)
-    PrimaryStat = playerStats.STR
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.STR, playerStats.CON]
-    Skills = stat2skill
-
-
-class Artificer:
-    HitDie = random.randrange(1, 8)
-    PrimaryStat = playerStats.INT
-    SpellSlots = 0
-    TotalCantrips = 0
-    Saves = [playerStats.CON, playerStats.INT]
-    Skills = stat2skill
+    class Artificer:
+        HitDie = random.randrange(1, 8)
+        PrimaryStat = playerStats.INT
+        SpellSlots = 0
+        TotalCantrips = 0
+        Saves = [playerStats.CON, playerStats.INT]
+        Skills = stat2skill
